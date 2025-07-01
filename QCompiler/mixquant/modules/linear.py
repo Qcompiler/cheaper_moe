fix scaling factor bug

QCompiler/mixquant/modules/linear.py
@@ -316,7 +316,7 @@ class MixLinear_GEMM(nn.Module):
 
             
         
-        # if y1[0,0] > 100:
+        # # if y1[0,0] > 100:
         # print(self.weight.shape)
         # print(self.weight)
         # print(self.bit)

QCompiler/mixquant/modules/mixed4bit.py
@@ -219,6 +219,9 @@ def gen_quant4_my(n, k, w, groupsize=-1,  tile = 1, bit = 4):
     if groupsize == -1:
         groupsize = k   
     maxq = 2 ** (bit) - 1  # 4-bit量化，最大值15
+
+    if bit <= 3:
+        maxq = 2 ** (3) - 1  # 缩放的时候缩放多一些
     n, k = w.shape  # 原始权重矩阵形状
 
     # 计算需要的组数（向上取整）
@@ -240,9 +243,11 @@ def gen_quant4_my(n, k, w, groupsize=-1,  tile = 1, bit = 4):
     linear = torch.clone(w_reshaped)
     linear = torch.round(linear / s).int()
 
+    linear = torch.clamp(linear,  - 2 ** (bit - 1), 2 ** (bit - 1) - 1)
+    
     pianyi = 2 ** (4) - 1  # 4-bit量化，最大值15
     linear += (pianyi + 1) // 2  # 添加零点偏移
-    linear = torch.clamp(linear, 0, maxq)
+
     # 将量化的权重和缩放因子重塑回原始形状
     linear = linear.reshape(n, -1)[:, :k]  # 移除填充并恢复原始k维度
     s = s.reshape(n, -1).contiguous()  # 缩放因子形状为 (n, num_groups)  